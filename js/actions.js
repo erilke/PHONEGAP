3e9d3fec91590948a194f41a7f6b53427cd3690f
erilke <erilkemx@gmail.com>
2015-09-19 12:31:23 -0500

    dia1

---
+++ js/actions.js
@@ -0,0 +1,13 @@
+var fn = {
+  init: function(){
+      if (!fn.islogged())
+          window.location.href="#reg";
+
+  },
+    islogged: function(){
+        return false;
+    }
+};
+
+
+window.addEventListener("load",fn.init, false);
