d014bbd609e0893d89202dff5ee1b360cc836556
erilke <erilkemx@gmail.com>
2015-09-26 12:21:11 -0500

    toma de foto

---
+++ js/API/mediaCapture.js
@@ -0,0 +1,21 @@
+var mediaCapture = {
+    takePhoto: function(){
+        //alert();
+        navigator.device.capture.captureImage(mediaCapture.captureSuccess, mediaCapture.captureError, {limit:2});
+     },
+
+    // capture callback
+    captureSuccess: function(mediaFiles) {
+        var i, path, len;
+        for (i = 0, len = mediaFiles.length; i < len; i++) {
+            path = mediaFiles[i].fullPath;
+        }
+        $('#regphoto').attr('rel',path).html('<img src="' + path +'" style="width:100%;">');
+    },
+    // capture error callback
+    captureError: function(error) {
+        navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error','Aceptar');
+    },
+    
+};
+
--- js/actions.js
+++ js/actions.js
@@ -2,12 +2,41 @@ var fn = {
   init: function(){
       if (!fn.islogged())
           window.location.href="#reg";
-
+    // funcionalidades de registro
+      
+      $('#regsend').click(fn.getRegister);
+    $('#takephoto').click(mediaCapture.takePhoto);
+      
   },
+    ready: function(){
+        document.addEventListener("deviceready", fn.init, false);
+    },
+    
     islogged: function(){
         return false;
-    }
+    },
+    
+    getRegister: function(){
+        var nom = $('#regname').val();
+        var mail = $('#regmail').val();
+        var tel = $('#regtel').val();
+        
+        //alert(nom + ' ' + mail + ' ' + tel);
+        if(nom != '' && tel != '' && mail != ''){
+            alert();
+            // enviar datos al servidor
+        }else{
+          // alert('Todos los campos son requeridos'); 
+            navigator.notification.alert('Todos los campos son requeirido',null, "Error de registro", "Aceptar");
+            
+    
+    },
+    
 };
 
 
-window.addEventListener("load",fn.init, false);
+//window.addEventListener("load",fn.ready, false);
+//jQuery(document).ready(fn.ready);
+//$(document).ready(fn.ready);
+$(fn.ready);
+//$(fn.init);
